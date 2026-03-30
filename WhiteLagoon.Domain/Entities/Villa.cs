8e1bb47addfd00a37139af1c634d7c416a12b2b7
Szymon <golden04m@wp.pl>
2026-03-30 19:58:05 +0200

    Sekcja 3

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -23,3 +23,4 @@ namespace WhiteLagoon.Domain.Entities
         public DateTime? UpdateDate { get; set; }
     }
 }
+
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -25,20 +25,67 @@ namespace WhiteLagoon.Web.Controllers
 
         [HttpPost]
         public IActionResult Create(Villa obj)
-        {   
-            if(obj.Name == obj.Description)
+        {
+            if (obj.Name == obj.Description)
             {
                 ModelState.AddModelError("description", "The description cannot exactly match the Name.");
             }
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View();
-            
+
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        { 
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
