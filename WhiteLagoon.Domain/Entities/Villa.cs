d84df4589cf92937e102a5d1f372e12e3e4df311
Szymon <golden04m@wp.pl>
2026-04-07 16:25:26 +0200

    Sekcja 8

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Domain.Entities
 {
@@ -17,6 +20,8 @@ namespace WhiteLagoon.Domain.Entities
         public int Sqft { get; set; }
         [Range(1,10)]
         public int Occupancy { get; set; }
+        [NotMapped]
+        public IFormFile? Image { get; set; }
         [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreateDate { get; set; }
